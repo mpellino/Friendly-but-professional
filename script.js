f2f42a6a4a57e56953ae5dd4577a4f229facf81f
mpellino <marcopellino00@gmail.com>
2023-02-14 13:40:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Get the button and the dropdown content
+
+const dropdownButtons = document.querySelectorAll(".link");
+const dropdownContents = document.querySelectorAll(".dropdown-menu");
+const image = document.querySelector(".portfolio-img");
+
+
+dropdownButtons.forEach(dropdownButton => {
+  let currentDropdownMenu = dropdownButton.parentNode.querySelector(".dropdown-menu");
+  dropdownButton.addEventListener("mouseenter", function(event) {
+    currentDropdownMenu.style.opacity = "1";
+    currentDropdownMenu.style.pointerEvents = "auto";
+  });
+
+  dropdownButton.addEventListener('mouseleave', function (event) {
+    currentDropdownMenu.style.opacity = '0';
+    currentDropdownMenu.style.pointerEvents = "none";
+  });
+})
+
+dropdownContents.forEach(dropdownContent => {
+  dropdownContent.addEventListener('mouseenter', function(event) {
+    let currentButton = event.relatedTarget
+    if (currentButton.className == "link") {
+      dropdownContent.style.opacity = '1'
+      dropdownContent.style.pointerEvents = "auto";
+    }
+  });
+  dropdownContent.addEventListener('mouseleave', function(event) {
+    dropdownContent.style.opacity = '0'
+    dropdownContent.style.pointerEvents = "none";
+  })
+})
+
