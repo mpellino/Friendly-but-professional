b2f85e69585be14d090192e2fab85e660458e858
mpellino <marcopellino00@gmail.com>
2023-02-16 15:13:49 +0100

    correct bug on first click.
    "When the app is first loaded, both the toggleFahrenheit and toggleCelsius divs have no style attribute. They are getting display rules from the CSS, true, but they have no style on themselves.
    
     So think about what your code sees, then. fahrenheit.style.display is null because that block doesn't have the style attribute yet. Therefore, fahrenheit.style.display === 'none' evaluates to false. As a result, the else block is executed and you end up displaying Celsius. Unfortunately, this is the default block which is shown, so the first click doesn't do anything.
    
     The second click works because after the code executes once, now both div blocks have a style attribute.
    
     To fix this, you should either put default style attributes onto the div tags or flip the logic in the code so you check on the Celsius block first, since that's the default display.
    
     Personally, I would use classes to toggle display behaviour instead. (https://stackoverflow.com/questions/45395138/the-onclick-is-not-working-on-first-click)"

--- script.js
+++ script.js
@@ -7,41 +7,18 @@ const image = document.querySelector(".portfolio-img");
 const mediaQuery = window.matchMedia("(max-width: 600px)");
 
 
-    dropdownButtons.forEach(dropdownButton => {
-      let currentDropdownMenu = dropdownButton.parentNode.querySelector(".dropdown-menu");
-      dropdownButton.addEventListener("mouseenter", function(event) {
-        currentDropdownMenu.style.opacity = "1";
-        currentDropdownMenu.style.pointerEvents = "auto";
-      });
 
-      dropdownButton.addEventListener('mouseleave', function (event) {
-        currentDropdownMenu.style.opacity = '0';
-        currentDropdownMenu.style.pointerEvents = "none";
-      });
-    })
-
-    dropdownContents.forEach(dropdownContent => {
-      dropdownContent.addEventListener('mouseenter', function(event) {
-        let currentButton = event.relatedTarget
-        if (currentButton.className == "link") {
-          dropdownContent.style.opacity = '1'
-          dropdownContent.style.pointerEvents = "auto";
-        }
-      });
-      dropdownContent.addEventListener('mouseleave', function(event) {
-        dropdownContent.style.opacity = '0'
-        dropdownContent.style.pointerEvents = "none";
-      })
-    });
 
 if (window.matchMedia("(max-width: 600px)").matches) {
 
   console.log("mobile");
   dropdownButtons.forEach(dropdownButton => {
     dropdownButton.addEventListener("click", function(event) {
-      event.preventDefault();
       let currentDropdownMenu = dropdownButton.parentNode.querySelector(".dropdown-menu");
-      if (currentDropdownMenu.style.opacity == "0") {
+      if (!currentDropdownMenu.style.opacity) {
+        currentDropdownMenu.style.opacity = "0";
+      }
+      if (currentDropdownMenu.style.opacity === "0") {
       currentDropdownMenu.style.opacity = "1";
       currentDropdownMenu.style.pointerEvents = "auto";
       } else {
@@ -49,6 +26,34 @@ if (window.matchMedia("(max-width: 600px)").matches) {
         currentDropdownMenu.style.pointerEvents = "none";
       }
     });
+  });
+} else {
+  console.log("desktop");
+  dropdownButtons.forEach(dropdownButton => {
+    let currentDropdownMenu = dropdownButton.parentNode.querySelector(".dropdown-menu");
+    dropdownButton.addEventListener("mouseenter", function(event) {
+      currentDropdownMenu.style.opacity = "1";
+      currentDropdownMenu.style.pointerEvents = "auto";
+    });
+
+    dropdownButton.addEventListener('mouseleave', function (event) {
+      currentDropdownMenu.style.opacity = '0';
+      currentDropdownMenu.style.pointerEvents = "none";
+    });
+  })
+
+  dropdownContents.forEach(dropdownContent => {
+    dropdownContent.addEventListener('mouseenter', function(event) {
+      let currentButton = event.relatedTarget
+      if (currentButton.className == "link") {
+        dropdownContent.style.opacity = '1'
+        dropdownContent.style.pointerEvents = "auto";
+      }
+    });
+    dropdownContent.addEventListener('mouseleave', function(event) {
+      dropdownContent.style.opacity = '0'
+      dropdownContent.style.pointerEvents = "none";
+    })
   });
 }
 
